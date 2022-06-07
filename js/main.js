68b7768000e17b154a64b10bb834d9313d492816
Evgenia <jenya.fenina@yandex.ru>
2022-06-07 16:50:03 +0300

    Add call getRandomIntegerNumberFromRange and getRandomNotIntegerNumberFromRange functions

--- js/main.js
+++ js/main.js
@@ -5,9 +5,12 @@ function getRandomIntegerNumberFromRange (min, max) {
   return Math.floor(Math.random() * (max - min) + 1) + min;
 }
 
+getRandomIntegerNumberFromRange(5, 10);
+
 function getRandomNotIntegerNumberFromRange (min, max, simbolsNumberAfterComma) {
   if (min >= max || min < 0 || max < 0) {
     throw 'Некорректно задан диапазон';
   }
   return ((Math.random() * (max - min)) + min).toFixed(simbolsNumberAfterComma);
 }
+getRandomNotIntegerNumberFromRange(1.1, 1.2, 7);
