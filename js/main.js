c08b5a09c2aaff51bad5b715bfe6b8cbd5eaa288
Evgenia <jenya.fenina@yandex.ru>
2022-06-16 15:54:21 +0300

    Fix comments

--- js/main.js
+++ js/main.js
@@ -1,58 +1,23 @@
-function getRandomIntegerNumberFromRange (min, max) {
-  const isRangeCorrect = min > max || min < 0 || max < 0;
-  if (isRangeCorrect) {
-    throw 'Некорректно задан диапазон';
-  }
-  if (min === max) {
-    return min;
-  }
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-getRandomIntegerNumberFromRange(1, 10);
-
-function getRandomNotIntegerNumberFromRange (min, max, simbolsNumberAfterComma) {
-  const isRangeCorrect = min > max || min < 0 || max < 0;
-  if (isRangeCorrect) {
-    throw 'Некорректно задан диапазон';
-  }
-  if (min === max) {
-    return min;
-  }
-  return +(((Math.random() * (max - min)) + min).toFixed(simbolsNumberAfterComma));
-}
-getRandomNotIntegerNumberFromRange(1.1, 1.2, 7);
-
-const getRandomArrayElement = (elements) => elements[getRandomIntegerNumberFromRange(0, elements.length - 1)];
-
-const MAX_LAT_OF_OFFER = 35.70000;
-const MIN_LAT_OF_OFFER = 35.65000;
-const MAX_LNG_OF_OFFER = 139.80000;
-const MIN_LNG_OF_OFFER = 139.70000;
-const MIN_PRICE_OF_OFFER = 1000;
-const MAX_PRICE_OF_OFFER = 10000;
-const MIN_ROOMS_OF_OFFER = 1;
-const MAX_ROOMS_OF_OFFER = 3;
-const MIN_GUESTS_OF_OFFER = 1;
-const MAX_GUESTS_OF_OFFER = 6;
-const ADRESS_OF_OFFER = [
-  `${getRandomNotIntegerNumberFromRange(MIN_LAT_OF_OFFER, MAX_LAT_OF_OFFER, 5)} ${getRandomNotIntegerNumberFromRange(MIN_LNG_OF_OFFER, MAX_LNG_OF_OFFER, 5)}`,
-  `${getRandomNotIntegerNumberFromRange(MIN_LAT_OF_OFFER, MAX_LAT_OF_OFFER, 5)} ${getRandomNotIntegerNumberFromRange(MIN_LNG_OF_OFFER, MAX_LNG_OF_OFFER, 5)}`,
-  `${getRandomNotIntegerNumberFromRange(MIN_LAT_OF_OFFER, MAX_LAT_OF_OFFER, 5)} ${getRandomNotIntegerNumberFromRange(MIN_LNG_OF_OFFER, MAX_LNG_OF_OFFER, 5)}`
-];
 const COUNT_OF_AD = 10;
-const createAvatarsArray = (count) => {
-  const avatars = [];
-  for (let i = 1; i <= count; i++) {
-    if (i < 10) {
-      i = `0${i}`;
-    }
-    avatars.push(`img/avatars/user${i}.png`);
-  }
-  return avatars;
+const COORDINATES_OF_OFFER = {
+  maxLat : 35.70000,
+  minLat : 35.65000,
+  maxLng : 139.80000,
+  minLng : 139.70000,
+  numberOfDigitsAfterComma : 5
 };
-const AVATAR_OF_AUTHOR = createAvatarsArray(COUNT_OF_AD);
-
+const PRICE_OF_OFFER = {
+  minPrice : 1000,
+  maxPrice : 10000
+};
+const ROOMS_OF_OFFER = {
+  minCountOfRooms : 1,
+  maxCountOfRooms : 3
+};
+const GUESTS_OF_OFFER =  {
+  minCountOfGuests : 1,
+  maxCountOfGuests : 6
+};
 const TITLE_OF_OFFER = [
   'My flat for you',
   'Be happy here',
@@ -92,35 +57,91 @@ const PHOTOS_OF_OFFER = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
 
-
-const createObj = (number) => ({
-  author : {
-    avatar : AVATAR_OF_AUTHOR[number],
-  },
-  offer : {
-    title : getRandomArrayElement(TITLE_OF_OFFER),
-    address : getRandomArrayElement(ADRESS_OF_OFFER),
-    price : getRandomIntegerNumberFromRange(MIN_PRICE_OF_OFFER, MAX_PRICE_OF_OFFER),
-    type : getRandomArrayElement(TYPE_OF_OFFER),
-    rooms : getRandomIntegerNumberFromRange(MIN_ROOMS_OF_OFFER, MAX_ROOMS_OF_OFFER),
-    guests : getRandomIntegerNumberFromRange(MIN_GUESTS_OF_OFFER, MAX_GUESTS_OF_OFFER),
-    checkin : getRandomArrayElement(CHECIN_OF_OFFER),
-    checkout : getRandomArrayElement(CHECKOUT_OF_OFFER),
-    features : getRandomArrayElement(FEATURES_OF_OFFER),
-    description : getRandomArrayElement(DESCRIPTION_OF_OFFER),
-    photos : getRandomArrayElement(PHOTOS_OF_OFFER)
-  },
-  location : {
-    lat : getRandomNotIntegerNumberFromRange(MIN_LAT_OF_OFFER, MAX_LAT_OF_OFFER, 5),
-    lng : getRandomNotIntegerNumberFromRange(MIN_LNG_OF_OFFER, MAX_LNG_OF_OFFER, 5)
+function getRandomIntegerNumberFromRange (min, max) {
+  const isRangeCorrect = min > max || min < 0 || max < 0;
+  if (isRangeCorrect) {
+    throw 'Некорректно задан диапазон';
   }
-});
+  if (min === max) {
+    return min;
+  }
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+function getRandomNotIntegerNumberFromRange (min, max, simbolsNumberAfterComma) {
+  const isRangeCorrect = min > max || min < 0 || max < 0;
+  if (isRangeCorrect) {
+    throw 'Некорректно задан диапазон';
+  }
+  if (min === max) {
+    return min;
+  }
+  return +(((Math.random() * (max - min)) + min).toFixed(simbolsNumberAfterComma));
+}
+
+const getRandomArrayElement = (elements) => elements[getRandomIntegerNumberFromRange(0, elements.length - 1)];
+
+const getAvatars = (count) => {
+  const avatars = [...Array(count)];
+  avatars.fill('').forEach((item, i) => {
+    avatars[i] = `img/avatars/user${String(i+1).padStart(2, '0')}.png`;
+  });
+  return avatars;
+};
+const AVATARS_OF_AUTHOR = getAvatars(COUNT_OF_AD);
+
+const getFeatures = (features) => {
+  const setOfFeaturesLength = getRandomIntegerNumberFromRange(1, features.length);
+  const setOfFeatures = [...Array(setOfFeaturesLength)];
+  const unicumFeatures = [...features];
+  setOfFeatures.filter((item, i) => {
+    const randomIndexUnicumFeatures = getRandomIntegerNumberFromRange(0, unicumFeatures.length - 1);
+    setOfFeatures[i] = unicumFeatures[randomIndexUnicumFeatures];
+    unicumFeatures.splice(randomIndexUnicumFeatures, 1);
+  });
+  return setOfFeatures;
+};
+
+const getPhotos = (photos) => {
+  const setOfPhotosLength = getRandomIntegerNumberFromRange(1, photos.length);
+  const setOfPhotos = [...Array(setOfPhotosLength)];
+  setOfPhotos.filter((item, i) => {
+    setOfPhotos[i] = photos[getRandomIntegerNumberFromRange(0, photos.length - 1)];
+  });
+  return setOfPhotos;
+};
+
+const createAds = (number) => {
+  const latOfOffer = getRandomNotIntegerNumberFromRange(COORDINATES_OF_OFFER.minLat, COORDINATES_OF_OFFER.maxLat, COORDINATES_OF_OFFER.numberOfDigitsAfterComma);
+  const lngOfOffer = getRandomNotIntegerNumberFromRange(COORDINATES_OF_OFFER.minLng, COORDINATES_OF_OFFER.maxLng, COORDINATES_OF_OFFER.numberOfDigitsAfterComma);
+  return {
+    author : {
+      avatar : AVATARS_OF_AUTHOR[number],
+    },
+    offer : {
+      title : getRandomArrayElement(TITLE_OF_OFFER),
+      address : `${latOfOffer}, ${lngOfOffer}`,
+      price : getRandomIntegerNumberFromRange(PRICE_OF_OFFER.minPrice, PRICE_OF_OFFER.maxPrice),
+      type : getRandomArrayElement(TYPE_OF_OFFER),
+      rooms : getRandomIntegerNumberFromRange(ROOMS_OF_OFFER.minCountOfRooms, ROOMS_OF_OFFER.maxCountOfRooms),
+      guests : getRandomIntegerNumberFromRange(GUESTS_OF_OFFER.minCountOfGuests, GUESTS_OF_OFFER.maxCountOfGuests),
+      checkin : getRandomArrayElement(CHECIN_OF_OFFER),
+      checkout : getRandomArrayElement(CHECKOUT_OF_OFFER),
+      features : getFeatures(FEATURES_OF_OFFER),
+      description : getRandomArrayElement(DESCRIPTION_OF_OFFER),
+      photos : getPhotos(PHOTOS_OF_OFFER)
+    },
+    location : {
+      lat : latOfOffer,
+      lng : lngOfOffer
+    }
+  };
+};
 
 const CreateArraySimilarAdsNearby = (count) => {
-  const similarAds = [];
-  for(let i = 0; i < count; i++) {
-    similarAds.push(createObj(i));
-  }
+  const similarAds = [...Array(count)];
+  similarAds.fill('').forEach((item, i) => {
+    similarAds[i] = createAds(i);
+  });
   return similarAds;
 };
 
