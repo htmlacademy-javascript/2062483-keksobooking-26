0df8258ed8e5278c1aa57bf693a634492fc95f24
Evgenia <jenya.fenina@yandex.ru>
2022-07-05 23:22:21 +0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 import {createOffers} from './data.js';
 import {COUNT_OF_AD} from './constants.js';
 import {createOfferCard} from './offer-card.js';
-import {
-  disablePage,
-  enablePage
-} from './form-activity-state.js';
+import {changeFormsState} from './toggle-status-page.js';
 
-disablePage();
+changeFormsState(false);
 const similarOffers = createOffers(COUNT_OF_AD);
 
 createOfferCard(similarOffers[0]);
-enablePage();
+
+changeFormsState(true);
