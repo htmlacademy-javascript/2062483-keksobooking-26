247b5e89b7b8038b002004b95e618331380d525a
Evgenia <jenya.fenina@yandex.ru>
2022-07-10 23:02:24 +0300

    Add validation ad form

--- js/constants.js
+++ js/constants.js
@@ -14,9 +14,29 @@ const FEATURES_OF_OFFER = [
   'elevator',
   'conditioner'
 ];
+const MAX_TITLE_LENGTH = 100;
+const MIN_TITLE_LENGTH = 30;
+const MAX_PRICE_VALUE = 100000;
+const RoomsCount = {
+  ONE_ROOM : '1',
+  TWO_ROOMS : '2',
+  THREE_ROOMS : '3',
+  HUNDRED_ROOMS : '100'
+};
+const GuestsCount = {
+  ONE_GUEST : '1',
+  TWO_GUESTS: '2',
+  THREE_GUESTS: '3',
+  UNAVAILABLE: '0'
+};
 
 export {
   COUNT_OF_AD,
   TYPE_OF_OFFER,
-  FEATURES_OF_OFFER
+  FEATURES_OF_OFFER,
+  MAX_TITLE_LENGTH,
+  MIN_TITLE_LENGTH,
+  MAX_PRICE_VALUE,
+  RoomsCount,
+  GuestsCount
 };
--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ import {createOffers} from './data.js';
 import {COUNT_OF_AD} from './constants.js';
 import {createOfferCard} from './offer-card.js';
 import {changeFormsState} from './toggle-status-page.js';
+import {getValidationAdForm} from './validation-ad-form.js';
+
+getValidationAdForm();
 
 changeFormsState(false);
 const similarOffers = createOffers(COUNT_OF_AD);
